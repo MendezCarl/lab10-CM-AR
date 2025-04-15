08eed5a9198ba84119047f82ef621c3023942fb2
MendezCar <mendez.c@ufl.edu>
2025-04-14 22:25:33 -0400

    did some changes

--- calculator.py
+++ calculator.py
@@ -7,7 +7,7 @@ calculator.py
 
 One function per operation, in order.
 """
-import math
+
 
 # First example
 import math
@@ -15,6 +15,7 @@ import math
 def square_root(a):
     if a < 0:
         raise ValueError("Number must not be negative")
+    return math.sqrt(a)
 
 def hypotenuse(a,b):
     return math.hypot(a,b)
@@ -36,7 +37,7 @@ def div(a, b):
 def log(a, b):
     if a <= 1:
         raise ValueError("Base cannot be zero")
-    return log(b, a)
+    return math.log(b, a)
 
 def exp(a, b):
     return a ** b
